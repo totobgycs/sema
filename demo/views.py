ef628f994ac63b491f420789a05177459e379d96
totobgycs <totobgycs@users.noreply.github.com>
2019-08-18 22:13:23 +0200

    Created demo app with welcome message and style  (#7) (#8)
    * Created demo app with welcome message
    
    * Technicalities
    
    * Technicalities: requirements

---
+++ demo/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+
+def index(request):
+    return render(
+      request,
+      'demo/index.html',
+      {'greeting': 'Proof of concept for a multi lingual chat app. For SEMA.'})

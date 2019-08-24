7219a54c5372459e206ecfb0858b0429e23a225f
totobgycs <totobgycs@users.noreply.github.com>
2019-08-24 09:40:38 +0200

    UI: Updated landing page and added a mockup for the demo (#9)
    * Created demo app with welcome message
    
    * Technicalities
    
    * Technicalities: requirements
    
    * User interface: a full mockup for the final demo, and an updated landing page

--- demo/views.py
+++ demo/views.py
@@ -1,8 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
 
+def mock1(request):
+    return render(
+      request,
+      'demo/mock1.html',
+      {})
+
 def index(request):
     return render(
       request,
       'demo/index.html',
       {'greeting': 'Proof of concept for a multi lingual chat app. For SEMA.'})
+

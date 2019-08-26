5553ed2487656fb9d919668b3e28d8bcfb053108
Ubuntu <ubuntu@ip-172-31-34-88.eu-west-1.compute.internal>
2019-08-26 19:59:52 +0000

    Using django templated to get the data from the server

--- demo/views.py
+++ demo/views.py
@@ -8,8 +8,25 @@ def mock1(request):
       {})
 
 def index(request):
+    messages = [
+      {
+        'timestamp':'19:58',
+        'translated_text':'SEMA is the Global Network of Victims and Survivors to End Wartime Sexual Violence.',
+        'note':'originally posted in English'
+       },
+      {
+        'timestamp':'20:10',
+        'translated_text':'SEMA literally means “Speak Out” in Swahili.',
+        'note': 'originally posted in English'
+      },
+      {
+        'timestamp':'20:12',
+        'translated_text':'There are survivors of wartime rape from 21 countries in Africa, South America, the Middle East, and Europe represented in the SEMA network.',
+        'note':'originally posted in English'
+        },
+      ]
     return render(
       request,
       'demo/index.html',
-      {'greeting': 'Proof of concept for a multi lingual chat app. For SEMA.'})
+      {'messages': messages})
 

a0daca924ad8b3e77fbfeef379c6f235c6b9f43a
totobgycs <totobgycs@users.noreply.github.com>
2019-09-02 19:24:50 +0200

    Created database model (#11)
    * Using django templated to get the data from the server
    
    * Created database models and made them editable from the admin surface

--- demo/models.py
+++ demo/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Language(models.Model):
+    # the language name in the original language, e.g. 'Magyar'
+    lang_native = models.CharField(max_length=30, unique=True)
+    # the language name in English, e.g. 'Hungarian'
+    lang_english = models.CharField(max_length=30, unique=True)
+    
+    def __str__(self):
+        return 'Language: '+self.lang_english
+
+
+class Message(models.Model):
+    timestamp = models.DateTimeField()
+    text_native = models.CharField(max_length=200)
+    lang_native = models.ForeignKey(Language, on_delete=models.PROTECT)
+    
+    def __str__(self):
+        return 'Message: '+self.text_native[0:40]+'...'
+
+
+class Translation(models.Model):
+    message = models.ForeignKey(Message, on_delete=models.PROTECT)
+    lang_target = models.ForeignKey(Language, on_delete=models.PROTECT)

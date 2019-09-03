14f62c7b0ea2095c9c3f0a6d6d9fbcdb146dda9a
Ubuntu <ubuntu@ip-172-31-34-88.eu-west-1.compute.internal>
2019-09-03 21:24:27 +0000

    Language selection

--- demo/urls.py
+++ demo/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<str:label>', views.language, name='language'),
     path('mock1', views.mock1, name='mock1'),
 ]
--- demo/views.py
+++ demo/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 
 from demo.models import *
 
@@ -10,25 +10,32 @@ def mock1(request):
     {})
 
 def index(request):
+  return redirect(language, label='English')
 
-  
+def language(request, label):
+
+  try:
+    language = Language.objects.get(lang_english=label)
+  except Language.DoesNotExist:
+    language = Language.objects.get(lang_english='English')
+
   languages = Language.objects.all()[:10]
-  dlanguages = [lang.lang_native + ' (' + lang.lang_english + ')' for lang in languages]
 
-  messages = Message.objects.all()
+  messages = Message.objects.all()[:100]
 
   dmessages = []
   for message in messages:
     dmessage = {}
     dmessage['timestamp'] = message.timestamp
     dmessage['translated_text'] = message.text_native
-    dmessage['note'] = "Raw data"
+    dmessage['note'] = "Original message (translation pending)"
     dmessages.append(dmessage) 
   return render(
     request,
     'demo/index.html',
     {
       'messages': dmessages,
-      'languages': dlanguages
+      'languages': languages,
+      'chosen_language': language,
     })
 
